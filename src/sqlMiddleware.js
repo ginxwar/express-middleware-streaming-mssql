4d476326dd9b3e9202e309db0ac2d7b26e13c89d
Kevin Wu <kevinwu@gdev10.gus.ucsb.edu>
2017-06-15 15:05:13 -0700

    forgot commas for row emitters

--- src/sqlMiddleware.js
+++ src/sqlMiddleware.js
@@ -1,6 +1,9 @@
 const mssql = require('mssql')
 
 const runSqlInPool = pool => sql => (req, res, next) => {
+  // keeps track of the row index.  The count is unknown because we are streaming
+  let rowIndex = 0
+
   const request = new mssql.Request(pool)
 
   //TODO: can we get information about the pool size and what connection number we are?
@@ -17,7 +20,13 @@ const runSqlInPool = pool => sql => (req, res, next) => {
   })
 
   request.on('row', row => {
+    if (rowIndex > 0) {
+      res.write(',')
+    }
+
     res.write(JSON.stringify(row))
+
+    rowIndex++
   })
 
   request.on('error', err => {
